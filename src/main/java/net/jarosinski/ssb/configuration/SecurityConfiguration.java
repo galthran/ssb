85935abff581afe947b1b8a6b4c796bb3eef1678
Galthran <rafal.jarosinski@gmail.com>
2024-01-15 20:50:48 +0100

    Adding SecurityConfiguration class, configure security

---
+++ src/main/java/net/jarosinski/ssb/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package net.jarosinski.ssb.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/contact", "/notices").permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults()
+        );
+        return http.build();
+    }
+}
