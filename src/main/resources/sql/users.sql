26f47d44b853c80b4b58d9ed27a07fab47c4ac17
Galthran <rafal.jarosinski@gmail.com>
2024-01-28 11:57:32 +0100

    Adding docker-compose file, adding sql script to create tables in the postgresql database, configuring connection to the postgresql database

---
+++ src/main/resources/sql/users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    username varchar(50) not null primary key,
+    password varchar(500) not null,
+    enabled boolean not null
+);
+
+create table authorities (
+	username varchar(50) not null,
+	authority varchar(50) not null,
+	constraint fk_authorities_users foreign key(username) references users(username)
+);
